45b5b7ca5d5b661f5d0b15cd4f9c69a321032834
League <jiwenzou@gmail.com>
2016-09-18 13:00:23 -0700

    did thingorinos
    meme

---
+++ src/Syso_input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Syso_input {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	String resp = s.next();	
+	
+	System.out.println("");
+	
+	if(resp.equals("hello")){
+		
+	}
+
+}
+}
