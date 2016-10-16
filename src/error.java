5dd42bd13c7f524faf00369801b110e5ed599c78
rylr <league@WTS9.attlocal.net>
2016-10-16 12:59:17 -0700

    nesting
    nested

---
+++ src/error.java
@@ -0,0 +1,9 @@
+public class error {
+	public static void main(String[] args) {
+		int number = 6;
+		for (int i = number; i > 0; i--) {
+			number--;
+			System.out.println(number);
+		}
+	}
+}
--- src/tresNumeros.java
+++ src/tresNumeros.java
@@ -8,42 +8,64 @@ public class tresNumeros {
 		int greatest = 0;
 		int middlest = 0;
 		int least = 0;
-		
+
 		int one = Integer.parseInt(num1);
 		int two = Integer.parseInt(num2);
 		int three = Integer.parseInt(num3);
-		
-		if(one < two && one < three){
-			least = one;
+
+		if (one < two) {
+			if (one < three) {
+				least = one;
+			}
 		}
-		if(one > two && one > three){
-			greatest = one;
+		if (one > two) {
+			if (one > three) {
+				greatest = one;
+			}
 		}
-		if(one > two && one < three || one > three && one < two){
+		if (one > two && one < three) {
 			middlest = one;
+			if (one > three && one < two) {
+				middlest = one;
+			}
 		}
-	
-		if(two < one && two < three){
-			least = two;
+
+		if (two < one) {
+			if (two < three) {
+				least = two;
+			}
 		}
-		if(two > one && two > three){
-			greatest = two;
+		if (two > one) {
+			if (two > three) {
+				greatest = two;
+			}
 		}
-		if(two > one && two < three || two > three && two < one){
+		if (two > one && two < three) {
 			middlest = two;
+			if (two > three && two < one) {
+				middlest = two;
+			}
+
 		}
-		
-		if(three < two && three < one){
-			least = three;
+
+		if (three < two) {
+			if (three < one) {
+				least = three;
+			}
 		}
-		if(three > two && three > one){
-			greatest = three;
+		if (three > two) {
+			if (three > one) {
+				greatest = three;
+			}
 		}
-		if(three > two && three < one || three < two && three > one){
+		if (three > two && three < one) {
 			middlest = three;
+			if (three < two && three > one) {
+				middlest = three;
+			}
 		}
-		
-		System.out.println(greatest + " " + middlest + " " +  least);
-		System.out.println(least + " " + middlest + " " +  greatest);
+
+		System.out.println(greatest + " " + middlest + " " + least);
+		System.out.println(least + " " + middlest + " " + greatest);
 	}
 }
