8f5c1ab635f84f28356261d605ba1a74f5ad9137
rylr <league@WTS9.attlocal.net>
2016-10-23 12:57:21 -0700

    commitment
    okay

--- src/FoorLoopGauntlet.java
+++ src/FoorLoopGauntlet.java
@@ -2,59 +2,57 @@
 public class FoorLoopGauntlet {
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 100; i++){
+		for (int i = 0; i < 100; i++) {
 			System.out.println(i);
 		}
-		for(int i = 100; i > 0; i--){
+		for (int i = 100; i > 0; i--) {
 			System.out.println(i);
 		}
-		for(int i = 2; i < 100; i +=2){
+		for (int i = 2; i < 100; i += 2) {
 			System.out.println(i);
 		}
-		for(int i = 1; i < 99; i+=2){
+		for (int i = 1; i < 99; i += 2) {
 			System.out.println(i);
 		}
-		for(int i = 1; i < 500; i++){
-			
-			if(i%2 == 0){
-			System.out.println(i + "even");
+		for (int i = 1; i < 500; i++) {
+
+			if (i % 2 == 0) {
+				System.out.println(i + "even");
 			} else {
-			System.out.println(i + "odd");
+				System.out.println(i + "odd");
 			}
 		}
-		for(int i = 0; i <112; i++){
-			System.out.println(i*7);
+		for (int i = 0; i < 112; i++) {
+			System.out.println(i * 7);
 		}
-		for(int i = 2001; i < 2016; i++){
-			System.out.println("in "  + i + " I was "  + (i - 2001)  + " year(s) old. " );
+		for (int i = 2001; i < 2016; i++) {
+			System.out.println("in " + i + " I was " + (i - 2001) + " year(s) old. ");
 		}
-		for(int i = 0; i < 3; i++){
-			for(int x = 0; x < 3; x++){
-			System.out.println(i + " " + x);
+		for (int i = 0; i < 3; i++) {
+			for (int x = 0; x < 3; x++) {
+				System.out.println(i + " " + x);
+			}
 		}
-		}
-		for(int x = 1; x < 10; x+=3){	
-					System.out.println(x+""+(x+1)+""+(x+2));		
+		for (int x = 1; x < 10; x += 3) {
+			System.out.println(x + "" + (x + 1) + "" + (x + 2));
 		}
 		System.out.println("");
-		
-		for(int i = 0; i < 10; i++){
-			for(int x = 0; x < 10; x++){
-				System.out.print(i+""+x + " ");
-				
-			} 
-		System.out.println(" ");
+
+		for (int i = 0; i < 10; i++) {
+			for (int x = 0; x < 10; x++) {
+				System.out.print(i + "" + x + " ");
+
+			}
+			System.out.println(" ");
 		}
-		
-		
-		for(int i = 0; i < 5; i++){
-			for(int x = 0; x < 5; x++){
+
+		for (int i = 0; i < 5; i++) {
+			for (int x = 0; x < i; x++) {
 				System.out.print("*");
 			}
 			System.out.println(" ");
 		}
-		
-		
+
 	}
 
 }
