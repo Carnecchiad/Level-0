ec2e083499ef07932015947da1c055cdad292391
rylr <league@WTS9.attlocal.net>
2016-10-30 12:58:30 -0700

    years
    d

--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,11 +1,16 @@
+import java.util.Calendar;
+
 import javax.swing.JOptionPane;
 
 public class CodingExercise2 {
 	public static void main(String[] args) {
+		Calendar now = Calendar.getInstance();
 
 		String j = JOptionPane.showInputDialog("How old are you?");
 		int i = Integer.parseInt(j);
-		JOptionPane.showMessageDialog(null, "you were born in the year " + (2016 - i));
+		now.add(Calendar.YEAR, -i);
+		int year = now.get(Calendar.YEAR);
+		JOptionPane.showMessageDialog(null, "you were born in the year " + (year));
 		if (i > 30) {
 			JOptionPane.showMessageDialog(null, "you're too old for this");
 		}
